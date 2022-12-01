b81d11340d84cd98c8cfaa81473da00a0f1c8d4e
EwoudPool <ewoud@odd.bot>
2022-12-01 23:45:38 +0100

    ok no numpy for day 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,16 +1,15 @@
 from inputs.converter import ListOfNumbersConverter
 from inputs.daily_input import DailyInput
-import numpy as np
 
 
 def day1_1(bags):
-    calories_per_bag = [np.sum(content) for content in bags]
-    return np.max(calories_per_bag)
+    calories_per_bag = [sum(content) for content in bags]
+    return max(calories_per_bag)
 
 
 def day1_2(bags):
-    calories_per_bag = [np.sum(content) for content in bags]
-    return np.sort(calories_per_bag)[-3:].sum()
+    calories_per_bag = [sum(content) for content in bags]
+    return sum(sorted(calories_per_bag)[-3:])
 
 
 if __name__ == '__main__':
